3d90c38b6f6ad1833997fe1a75cd613492340054
Ben Miles <bcgm3studios@gmail.com>
2024-10-14 12:34:33 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# jeopardy-scorekeeper
+ Play along with Jeopardy on your smartphone -- Buzz in, keep score, and compete against your friends.
---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+export default function App() {
+  const [count, setCount] = useState(0)
+//   const [count, setCount] = useState(0)
+const sound = new Audio("sound.mp3");
+const playSound = () => { 
+	sound.play(); 
+	sound.currentTime = 0;
+}
+
+  return (
+    <>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={playSound}>Buzz In</button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}

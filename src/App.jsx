7a9de0d0eef7c76aaaf39f9d7a3680059f0b9376
Ben Miles <bcgm3studios@gmail.com>
2024-10-16 14:31:15 -0400

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,15 @@ export default function App() {
   const [count, setCount] = useState(0)
 //   const [count, setCount] = useState(0)
 
-  return (
-  )
 	const sound = new Audio("sound.mp3");
 	const buzzIn = () => {
 		sound.play(); 
 		sound.currentTime = 0;
 		// TODO: SHOW the points array and correct/incorrect buttons
 	}
+	return (
+		<>
 			<button onClick={buzzIn}>Buzz In</button>
+		</>
+	)
 }

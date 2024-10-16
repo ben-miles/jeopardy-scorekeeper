350f42da3ade6dc23ce9347f61ed9ca9e5d672cd
Ben Miles <bcgm3studios@gmail.com>
2024-10-16 14:33:07 -0400

    MVP Possible Points

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,39 @@ import { useState } from 'react'
 import './App.css'
 
 export default function App() {
-  const [count, setCount] = useState(0)
-//   const [count, setCount] = useState(0)
-
+	const points = [200, 400, 600, 800, 1000]
 	const sound = new Audio("sound.mp3");
+	const [thisPoints, setThisPoints] = useState(0);
 	const buzzIn = () => {
 		sound.play(); 
 		sound.currentTime = 0;
 		// TODO: SHOW the points array and correct/incorrect buttons
 	}
+	const onChangeValue = (e) => {
+		setThisPoints(Number(e.target.value));
+	}
 	return (
 		<>
 			<button onClick={buzzIn}>Buzz In</button>
+			<div id="points">
+				<fieldset onChange={onChangeValue}>
+					<legend>Points for this Clue:</legend>
+					{points.map((point, index) => (
+					<div key={index}>
+						<label>
+							<input 
+								type="radio" 
+								id={"point-" + point} 
+								name="points" 
+								value={point}
+								// defaultChecked={index === 0}
+							/>
+							${point}
+						</label>
+					</div>
+					))}
+				</fieldset>
+			</div>
 		</>
 	)
 }

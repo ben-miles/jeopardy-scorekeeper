9acddd69089c0140aba76b099dfaf1668eb03978
Ben Miles <bcgm3studios@gmail.com>
2024-10-16 14:34:15 -0400

    MVP Scoring

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import './App.css'
 export default function App() {
 	const points = [200, 400, 600, 800, 1000]
 	const sound = new Audio("sound.mp3");
+	const [score, setScore] = useState([]);
 	const [thisPoints, setThisPoints] = useState(0);
 	const buzzIn = () => {
 		sound.play(); 
@@ -12,6 +13,15 @@ export default function App() {
 	}
 	const onChangeValue = (e) => {
 		setThisPoints(Number(e.target.value));
+	}
+	const sum = score.reduce((partialSum, a) => partialSum + a, 0);
+	const handleClick = (operator) => () => {
+		if (thisPoints === 0) {
+			alert('Please select a point value');
+			return;
+		}
+		setScore(score => [...score, operator === '+' ? thisPoints : thisPoints * -1]);
+		// TODO: HIDE the points array and correct/incorrect buttons
 	}
 	return (
 		<>
@@ -35,6 +45,21 @@ export default function App() {
 					))}
 				</fieldset>
 			</div>
+			<div id="addsub">
+				<fieldset>
+					<legend>Your Response:</legend>
+					<button onClick={handleClick('+')}>Correct</button>
+					<button onClick={handleClick('-')}>Incorrect</button>
+				</fieldset>
+			</div>
+			<div id="history">
+				<ul>
+					{score.map((score, index) => (
+					<li key={index}>{score}</li>
+					))}
+				</ul>
+			</div>
+			<div id="score">{sum}</div>
 		</>
 	)
 }

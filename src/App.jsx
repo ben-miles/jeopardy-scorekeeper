4e3423eb4cb97cf46c036b663bf5f2cf29d744cd
Ben Miles <bcgm3studios@gmail.com>
2024-10-16 12:46:18 -0400

    MVP Buzzer
    Bare minimum functionality for the "buzzer"

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import './App.css'
 export default function App() {
   const [count, setCount] = useState(0)
 //   const [count, setCount] = useState(0)
-const sound = new Audio("sound.mp3");
-const playSound = () => { 
-	sound.play(); 
-	sound.currentTime = 0;
-}
 
   return (
   )
+	const sound = new Audio("sound.mp3");
+	const buzzIn = () => {
+		sound.play(); 
+		sound.currentTime = 0;
+		// TODO: SHOW the points array and correct/incorrect buttons
+	}
+			<button onClick={buzzIn}>Buzz In</button>
 }

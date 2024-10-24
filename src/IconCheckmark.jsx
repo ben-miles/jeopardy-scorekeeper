9e40aaa5618f0777a4481a2629969c22f399c4cc
Ben Miles <bcgm3studios@gmail.com>
2024-10-24 16:28:48 -0400

    Add Classes to SVG Icon Components

--- src/IconCheckmark.jsx
+++ src/IconCheckmark.jsx
@@ -1,6 +1,6 @@
-export default function IconMenu() {
+export default function IconCheckmark() {
 	return (
-		<svg viewBox="0 0 64 64">
+		<svg className="icon icon-x" viewBox="0 0 64 64">
 			<polygon points="52.015,8.758 22.5,38.271 11.985,27.758 3.5,36.242 22.5,55.242 60.5,17.243 "/>
 		</svg>
 	)
--- src/IconMenu.jsx
+++ src/IconMenu.jsx
@@ -1,6 +1,6 @@
 export default function IconMenu() {
 	return (
-		<svg viewBox="0 0 64 64">
+		<svg className="icon icon-menu" viewBox="0 0 64 64">
 			<g>
 				<rect y="7" width="64" height="12"/>
 				<rect y="45" width="64" height="12"/>

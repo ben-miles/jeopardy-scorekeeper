67a4970e06e439386b287ec771dc0e23688c0cb5
Ben Miles <bcgm3studios@gmail.com>
2024-10-24 15:30:49 -0400

    Create React Components from SVGs

---
+++ src/IconCheckmark.jsx
@@ -0,0 +1,7 @@
+export default function IconMenu() {
+	return (
+		<svg viewBox="0 0 64 64">
+			<polygon points="52.015,8.758 22.5,38.271 11.985,27.758 3.5,36.242 22.5,55.242 60.5,17.243 "/>
+		</svg>
+	)
+}
---
+++ src/IconMenu.jsx
@@ -0,0 +1,11 @@
+export default function IconMenu() {
+	return (
+		<svg viewBox="0 0 64 64">
+			<g>
+				<rect y="7" width="64" height="12"/>
+				<rect y="45" width="64" height="12"/>
+				<rect y="26" width="64" height="12"/>
+			</g>
+		</svg>
+	)
+}

9dff93e49bd5c55dde8da70bb8f86ea19c058f54
Ben Miles <bcgm3studios@gmail.com>
2024-10-25 13:06:51 -0400

    Add Aria Attributes to SVG Components
    This will prevent the icons themselves from being focusable in screenreaders, while allowing the corresponding visually-hidden text labels to be focusable.

--- src/IconCheckmark.jsx
+++ src/IconCheckmark.jsx
@@ -1,6 +1,6 @@
 export default function IconCheckmark() {
 	return (
-		<svg className="icon icon-x" viewBox="0 0 64 64">
+		<svg className="icon icon-x" aria-hidden="true" focusable="false" viewBox="0 0 64 64">
 			<polygon points="52.015,8.758 22.5,38.271 11.985,27.758 3.5,36.242 22.5,55.242 60.5,17.243 "/>
 		</svg>
 	)
--- src/IconX.jsx
+++ src/IconX.jsx
@@ -1,6 +1,6 @@
 export default function IconX() {
 	return (
-		<svg className="icon icon-x" viewBox="0 0 64 64">
+		<svg className="icon icon-x" aria-hidden="true" focusable="false" viewBox="0 0 64 64">
 			<polygon points="58.854,13.615 50.369,5.13 31.984,23.515 13.6,5.13 5.114,13.615 23.499,32 5.114,50.385 13.6,58.87 31.984,40.485 
 				50.369,58.87 58.854,50.385 40.47,32 "/>
 		</svg>
